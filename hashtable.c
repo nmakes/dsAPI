ff63771f62bb1aff99e08c508c0cdb6d22729998
nmakes <nav.naveenvenkat@gmail.com>
2018-06-12 00:03:24 +0530

    working on hashtable

---
+++ hashtable.c
@@ -0,0 +1,25 @@
+#include "hashtable.h"
+
+// ---
+
+#define _HASHTABLE_SIZE_TYPE long long int
+
+struct _HASHTABLE
+{
+    _HASHTABLE_SIZE_TYPE size;
+    int multi;
+    void * elems;
+};
+
+typedef struct _HASHTABLE * hashtable;
+
+// ---
+
+hashtable hashtableMake(_HASHTABLE_SIZE_TYPE size, int multi);
+
+hashtable hashtableFree(hashtable s);
+int setExists(set s, _HASHTABLE_SIZE_TYPE elem);
+int setAdd(set s, _HASHTABLE_SIZE_TYPE elem);
+int setDel(set s, _HASHTABLE_SIZE_TYPE elem);
+set setRemake(set s, _HASHTABLE_SIZE_TYPE size, int multi);
+int setPrint(set s);
---
+++ hashtable.h
@@ -0,0 +1,18 @@
+#ifndef __HASHTABLE_H_API
+#define __HASHTABLE_H_API
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#define _SET_SIZE_TYPE long long int
+
+struct _HASHTABLE
+{
+    _SET_SIZE_TYPE size;
+    void * elems;
+};
+
+typedef struct _HASHTABLE * hashtable;
+
+
+#endif
